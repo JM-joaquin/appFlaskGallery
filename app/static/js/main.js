b5d089b57087b83350c833ee0c0ede753f69ab1f
joaquin monzon <joamonz15@gmail.com>
2023-07-06 11:32:34 -0300

    fixs

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,3 @@
-const EXTENCIONES = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp'];
-const api = "http://127.0.0.1:3000";
-let imgContainer = document.getElementById("imgs");
-
 const addImg = () => {
     const file = document.getElementById("file-img");
     const archivo = file.files[0];
@@ -16,6 +12,8 @@ const addImg = () => {
         .then(response => {
           if (response.ok) {
             alert('Imagen enviada exitosamente');
+            mostrarImg();
+            file.value = "";
           } else {
             throw new Error('Error al enviar la imagen');
           }
@@ -39,11 +37,11 @@ const obtenerImgs = async () => {
 
 
 const deleteImg = (name) => {
-  console.log(name);
   fetch(`/delete_img/${name}`)
     .then(response =>{
       if (response.ok) {
         alert('Imagen se elimino exitosamente');
+        mostrarImg();
       } else {
         throw new Error('Error al eliminar la imagen');
       }
@@ -55,13 +53,31 @@ const deleteImg = (name) => {
 
 const mostrarImg = async () => {
   const imgs = await obtenerImgs();
-  imgs.forEach(element => {
+  const imgContainer = document.getElementById("imgs");
+  imgContainer.innerHTML = "";
+  imgs.forEach((element) => {
+    const modalId = `eModal-${element}`; //
     imgContainer.innerHTML += `
     <div class="gallery">
-      ${element}
-      <br>
-      <button onclick="deleteImg('${element}')" type="button" class="btn-close btn-close-white position-absolute z-3" aria-label="Close"></button>
+      <button type="button" class="bg-danger btn-close position-absolute z-3" aria-label="Close" data-toggle="modal" data-target="#${modalId}"></button>
       <img src="../static/imgs/${element}" alt="imagen">
+    </div>
+
+    <div id="${modalId}" class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">¿Seguro que quiere eliminarlo?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-footer">
+            <button onclick="deleteImg('${element}')" type="button" class="btn btn-danger" data-dismiss="modal">Eliminar</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+          </div>
+        </div>
+      </div>
     </div>
     `
   });
